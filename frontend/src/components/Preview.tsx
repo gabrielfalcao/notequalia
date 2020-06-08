8f97882c5fa5894885541b6592c35448b5a832eb
Gabriel Falcão <gabriel@nacaolivre.org>
2020-06-08 02:38:08 +0200

    theme

--- frontend/src/components/Preview.tsx
+++ frontend/src/components/Preview.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import marked from "marked";
 import Col from "react-bootstrap/Col";
-//import "bootswatch/dist/journal/bootstrap.min.css";
+import "bootswatch/dist/journal/bootstrap.min.css";
 
 interface Props {
     markdownContent: string;
