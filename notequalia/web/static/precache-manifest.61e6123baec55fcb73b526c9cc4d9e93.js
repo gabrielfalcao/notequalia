237415d6b1e63de66d896802c2e7105afcc4d257
Gabriel Falcão <gabriel@nacaolivre.org>
2020-06-16 12:07:07 +0200

    new release of react-app

---
+++ notequalia/web/static/precache-manifest.61e6123baec55fcb73b526c9cc4d9e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e5500cccf32fea2f3ca303dc27d79f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e659eeee2c072407b631",
+    "url": "/static/css/2.5df535b6.chunk.css"
+  },
+  {
+    "revision": "e659eeee2c072407b631",
+    "url": "/static/js/2.9576e0a6.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.9576e0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7610b8f2dd6a9ee0f0e0",
+    "url": "/static/js/main.c8596c83.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
