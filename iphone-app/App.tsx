4ce158917c868877c37d8a32e9952e1ab1d5f340
Gabriel Falcão <gabriel@nacaolivre.org>
2020-06-21 00:15:31 +0200

    add a fabulous "Fab" fan

--- iphone-app/App.tsx
+++ iphone-app/App.tsx
@@ -6,6 +6,7 @@ import { enableScreens } from "react-native-screens";
 
 import { createNativeStackNavigator } from "react-native-screens/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { Container, Header, View, Button, Icon, Fab } from "native-base";
 
 import { AuthPropTypes } from "./domain/auth";
 
@@ -19,12 +20,72 @@ const AppPropTypes = {
 };
 
 type AppProps = InferProps<typeof AppPropTypes> | any;
+type AppState = {
+    active: boolean;
+};
 
 enableScreens();
 const Stack = createNativeStackNavigator();
 
+class AppLayout extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
+
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Lexicon">
+                    <Stack.Screen name="Lexicon" component={Home} />
+                    <Stack.Screen
+                        name="WordDefinition"
+                        component={WordDefinition}
+                        options={({ route }) => ({
+                            title: `Term: "${route.params.termName}"`
+                        })}
+                    />
+                </Stack.Navigator>
+
+                <Fab
+                    active={this.state.active}
+                    direction="up"
+                    containerStyle={{}}
+                    style={{ backgroundColor: "#2c3e50" }}
+                    position="bottomRight"
+                    onPress={() =>
+                        this.setState({ active: !this.state.active })
+                    }
+                >
+                    <Icon type="SimpleLineIcons" name="screen-tablet" />
+                    <Button style={{ backgroundColor: "#8e44ad" }}>
+                        <Icon type="MaterialCommunityIcons" name="graphql" />
+                    </Button>
+                    <Button style={{ backgroundColor: "#d35400" }}>
+                        <Icon
+                            type="MaterialCommunityIcons"
+                            name="textbox-password"
+                        />
+                    </Button>
+                    <Button
+                        style={{
+                            backgroundColor: "#2980b9"
+                        }}
+                    >
+                        <Icon
+                            type="MaterialCommunityIcons"
+                            name="note-multiple"
+                        />
+                    </Button>
+                </Fab>
+            </NavigationContainer>
+        );
+    }
+}
 const AppContainer = connect<AppProps>(
-    (state: any) => {
+    (state: AppState) => {
         return { ...state };
     },
     {
@@ -35,20 +96,7 @@ const AppContainer = connect<AppProps>(
             };
         }
     }
-)(({ }) => (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName="Lexicon">
-            <Stack.Screen name="Lexicon" component={Home} />
-            <Stack.Screen
-                name="WordDefinition"
-                component={WordDefinition}
-                options={({ route }) => ({
-                    title: `Term: "${route.params.termName}"`
-                })}
-            />
-        </Stack.Navigator>
-    </NavigationContainer>
-));
+)(AppLayout);
 
 const App = () => (
     <Provider store={store}>
