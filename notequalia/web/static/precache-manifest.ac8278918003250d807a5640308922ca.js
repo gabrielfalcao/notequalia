8393ffc551bb8d77fd920765480b5db57f1479f1
Gabriel Falcão <gabriel@nacaolivre.org>
2020-09-22 03:28:43 +0200

    new release of react-app

---
+++ notequalia/web/static/precache-manifest.ac8278918003250d807a5640308922ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26bc15728088ed3f2dcfaf976131f51f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b71319c33dd4595e2c3",
+    "url": "/static/css/2.7935454b.chunk.css"
+  },
+  {
+    "revision": "1b71319c33dd4595e2c3",
+    "url": "/static/js/2.fbfd6df7.chunk.js"
+  },
+  {
+    "revision": "019ce8002ca3f342bc95c170b139a87c",
+    "url": "/static/js/2.fbfd6df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce8088f38a84dbe87a5",
+    "url": "/static/js/main.e8546ceb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- notequalia/web/static/service-worker.js
+++ notequalia/web/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d54ec9d329b8e20847fa7d98d1017e6.js"
+  "/precache-manifest.ac8278918003250d807a5640308922ca.js"
 );
 
 self.addEventListener('message', (event) => {
