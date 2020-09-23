2416384c9e1685ddaa406b28541127a4133b30b0
Gabriel Falcão <gabriel@nacaolivre.org>
2020-09-23 21:08:10 +0200

    termsearch within termview

--- web-app/src/pages/ViewTerm.tsx
+++ web-app/src/pages/ViewTerm.tsx
@@ -22,179 +22,188 @@ import Col from "react-bootstrap/Col";
 import { AuthPropTypes } from "../domain/auth";
 import { TermProps } from "../domain/terms";
 import { TermsReducerState, TermListState } from "../reducers/types";
+import TermSearch from "../components/TermSearch";
 import Error from "../components/Error";
 import { DictionaryAPIClient } from "../networking";
 
 const ViewTermPropTypes = {
-    addError: PropTypes.func,
-    addTerms: PropTypes.func,
-    auth: AuthPropTypes
+	addError: PropTypes.func,
+	addTerms: PropTypes.func,
+	auth: AuthPropTypes
 };
 
 type MatchParams = {
-    termID: string;
+	termID: string;
 };
 
 type ViewTermProps =
-    | (RouteComponentProps<MatchParams> & {
-        terms: TermsReducerState;
-    } & InferProps<typeof ViewTermPropTypes>)
-    | any;
+	| (RouteComponentProps<MatchParams> & {
+			terms: TermsReducerState;
+	  } & InferProps<typeof ViewTermPropTypes>)
+	| any;
 
 class ViewTerm extends Component<ViewTermProps, any> {
-    private api: DictionaryAPIClient;
-    constructor(props: ViewTermProps) {
-        super(props);
-        const { addError } = props;
-        this.api = new DictionaryAPIClient(
-            addError,
-            props.auth.access_token.content
-        );
-    }
+	private api: DictionaryAPIClient;
+	constructor(props: ViewTermProps) {
+		super(props);
+		const { addError } = props;
+		this.api = new DictionaryAPIClient(
+			addError,
+			props.auth.access_token.content
+		);
+	}
 
-    render() {
-        const { terms, match }: ViewTermProps = this.props;
+	render() {
+		const { terms, match }: ViewTermProps = this.props;
 
-        if (!match) {
-            return <Error message="failed to parse term id from url" />;
-        }
-        const { termID } = match.params;
-        const term: TermProps = terms.by_term[termID];
+		if (!match) {
+			return <Error message="failed to parse term id from url" />;
+		}
+		const { termID } = match.params;
+		const term: TermProps = terms.by_term[termID];
 
-        if (!term) {
-            return <Redirect to="/" />;
-        }
-        const meta: any = term.content;
+		if (!term) {
+			return <Redirect to="/" />;
+		}
+		const meta: any = term.content;
 
-        const { collegiate, thesaurus } = meta;
+		const { collegiate, thesaurus } = meta;
 
-        if (!collegiate && !thesaurus) {
-            return null;
-        }
+		if (!collegiate && !thesaurus) {
+			return null;
+		}
 
-        return (
-            <Container fluid>
-                <Row>
-                    <Col md={12}>
-                        <Modal.Dialog>
-                            <Modal.Header>
-                                <Modal.Title>{term.term}</Modal.Title>
-                            </Modal.Header>
+		return (
+			<Container fluid>
+				<Row>
+					<Col md={12}>
+						<Modal.Dialog>
+							<Modal.Header>
+								<Modal.Title>{term.term}</Modal.Title>
+							</Modal.Header>
 
-                            <Modal.Body>
-                                {thesaurus ? (
-                                    <ListGroup variant="flush">
-                                        {thesaurus.map(
-                                            (
-                                                definition: any,
-                                                index: number
-                                            ) => {
-                                                const short_definitions: any =
-                                                    definition.short;
-                                                return (
-                                                    <ListGroup.Item
-                                                        key={`${index}`}
-                                                    >
-                                                        <h4>
-                                                            {
-                                                                definition.functional_label
-                                                            }
-                                                        </h4>
-                                                        {short_definitions.map(
-                                                            (
-                                                                description: any,
-                                                                index: number
-                                                            ) => (
-                                                                    <ListGroup.Item
-                                                                        key={`${index}`}
-                                                                    >
-                                                                        <h5>
-                                                                            {
-                                                                                description
-                                                                            }
-                                                                        </h5>
-                                                                    </ListGroup.Item>
-                                                                )
-                                                        )}
-                                                    </ListGroup.Item>
-                                                );
-                                            }
-                                        )}
-                                    </ListGroup>
-                                ) : null}
-                                {collegiate ? (
-                                    <ListGroup variant="flush">
-                                        {collegiate.map(
-                                            (
-                                                definition: any,
-                                                index: number
-                                            ) => {
-                                                const short_definitions: any =
-                                                    definition.short;
-                                                return (
-                                                    <ListGroup.Item
-                                                        key={`${index}`}
-                                                    >
-                                                        <h4>
-                                                            {
-                                                                definition.functional_label
-                                                            }
-                                                        </h4>
-                                                        {short_definitions.map(
-                                                            (
-                                                                description: any,
-                                                                index: number
-                                                            ) => (
-                                                                    <ListGroup.Item
-                                                                        key={`${index}`}
-                                                                    >
-                                                                        <h5>
-                                                                            {
-                                                                                description
-                                                                            }
-                                                                        </h5>
-                                                                    </ListGroup.Item>
-                                                                )
-                                                        )}
-                                                    </ListGroup.Item>
-                                                );
-                                            }
-                                        )}
-                                    </ListGroup>
-                                ) : null}
-                            </Modal.Body>
+							<Modal.Body>
+								{thesaurus ? (
+									<ListGroup variant="flush">
+										{thesaurus.map(
+											(
+												definition: any,
+												index: number
+											) => {
+												const short_definitions: any =
+													definition.short;
+												return (
+													<ListGroup.Item
+														key={`${index}`}
+													>
+														<h4>
+															{
+																definition.functional_label
+															}
+														</h4>
+														{short_definitions.map(
+															(
+																description: any,
+																index: number
+															) => (
+																<ListGroup.Item
+																	key={`${index}`}
+																>
+																	<h5>
+																		{
+																			description
+																		}
+																	</h5>
+																</ListGroup.Item>
+															)
+														)}
+													</ListGroup.Item>
+												);
+											}
+										)}
+									</ListGroup>
+								) : null}
+								{collegiate ? (
+									<ListGroup variant="flush">
+										{collegiate.map(
+											(
+												definition: any,
+												index: number
+											) => {
+												const short_definitions: any =
+													definition.short;
+												return (
+													<ListGroup.Item
+														key={`${index}`}
+													>
+														<h4>
+															{
+																definition.functional_label
+															}
+														</h4>
+														{short_definitions.map(
+															(
+																description: any,
+																index: number
+															) => (
+																<ListGroup.Item
+																	key={`${index}`}
+																>
+																	<h5>
+																		{
+																			description
+																		}
+																	</h5>
+																</ListGroup.Item>
+															)
+														)}
+													</ListGroup.Item>
+												);
+											}
+										)}
+									</ListGroup>
+								) : null}
+							</Modal.Body>
 
-                            <Modal.Footer>
-                                <LinkContainer to={`/`}>
-                                    <Button variant="warning">Back{""}</Button>
-                                </LinkContainer>
-                            </Modal.Footer>
-                        </Modal.Dialog>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
+							<Modal.Footer>
+								<LinkContainer to={`/`}>
+									<Button variant="warning">Back{""}</Button>
+								</LinkContainer>
+							</Modal.Footer>
+						</Modal.Dialog>
+					</Col>
+				</Row>
+				<Row>
+					<Col md={6}>
+						<h1>Search another term</h1>
+					</Col>
+					<Col md={6}>
+						<TermSearch />
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
 }
 
 export default withRouter(
-    connect<ViewTermProps>(
-        state => {
-            return { ...state };
-        },
-        {
-            addTerms: function(terms: TermListState[]) {
-                return {
-                    type: "ADD_TERMS",
-                    terms
-                };
-            },
-            addError: function(error: Error) {
-                return {
-                    type: "ADD_ERROR",
-                    error
-                };
-            }
-        }
-    )(ViewTerm)
+	connect<ViewTermProps>(
+		state => {
+			return { ...state };
+		},
+		{
+			addTerms: function(terms: TermListState[]) {
+				return {
+					type: "ADD_TERMS",
+					terms
+				};
+			},
+			addError: function(error: Error) {
+				return {
+					type: "ADD_ERROR",
+					error
+				};
+			}
+		}
+	)(ViewTerm)
 );
