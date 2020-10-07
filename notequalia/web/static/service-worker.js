27473551fc39d99945bc268ca52673134a8afcc9
Gabriel Falcão <gabriel@nacaolivre.org>
2020-10-07 04:00:18 +0200

    new release of react-app

--- notequalia/web/static/service-worker.js
+++ notequalia/web/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bdf5b2ce2483a4a93e192b708cf8d4e.js"
+  "/precache-manifest.f420ba309dd75ebc39defc5943d9690c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
